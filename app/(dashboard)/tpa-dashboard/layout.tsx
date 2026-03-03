2e9351217ab24084a71003921b3f48b3cbdbcec9
princeBrumainc <Prince.patel@brumainc.com>
2026-03-04 01:09:58 +0530

    added user view

--- app/(dashboard)/tpa-dashboard/layout.tsx
+++ app/(dashboard)/tpa-dashboard/layout.tsx
@@ -1,16 +1,18 @@
 import "@/app/components/global.css"
 import { inter } from "@/app/components/ui/fonts";
 import Header from "@/app/components/dashboard/header";
+import { getAuthenticatedUser } from "@/app/lib/dal";
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const user = await getAuthenticatedUser();
     return (
         <>
             <div className="max-w-7xl mx-6 px-6">
-                <Header userRole="tpa" userName="Admin User" notificationCount={3} />
+            <Header userRole={user.role as 'tpa' | 'sponsor'} userName={user.name} notificationCount={3} />
             </div>
             {children}
         </>
